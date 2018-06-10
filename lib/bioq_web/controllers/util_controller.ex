468f4c421ef301c0e174e5dd2996fe0ff3a1c18b
Trang Tran <ttdtrang@gmail.com>
2018-06-10 15:55:52 -0600

    rgen output rendering

--- lib/bioq_web/controllers/util_controller.ex
+++ lib/bioq_web/controllers/util_controller.ex
@@ -6,9 +6,17 @@ defmodule BioqWeb.UtilController do
   end
 
   def rgen(conn, %{"rgen_params" => rgen_params}) do
-    text conn, ~s[distribution: #{Map.get(rgen_params,"distribution")}
-  size: #{Map.get(rgen_params,"size")}]
+    # text conn, ~s[distribution: #{Map.get(rgen_params,"distribution")}
+    #      size: #{Map.get(rgen_params,"size")}]
+    # run the function
+    # send the result back for rendering
+    paramText = Enum.map(Map.keys(rgen_params),fn(x) -> ~s/#{x}: #{Map.get(rgen_params,x)}/ end)
+      |> Enum.join(", ")
+    output = [3,2,1,4,5,6,6,3,3,2]
+    outputText = Enum.join(output, ", ")
+    render conn, "rgen-output.html", %{"output": outputText , "params": paramText}
   end
+
   def rgen(conn, _params) do
     render conn, "rgen.html"
   end
