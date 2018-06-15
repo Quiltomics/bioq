53903af2724542d62ed91b50451224f79da917aa
Trang Tran <ttdtrang@gmail.com>
2018-06-15 00:17:08 -0600

    restful url

--- lib/bioq_web/controllers/util_controller.ex
+++ lib/bioq_web/controllers/util_controller.ex
@@ -10,13 +10,17 @@ defmodule BioqWeb.UtilController do
     #      size: #{Map.get(rgen_params,"size")}]
     # run the function
     # send the result back for rendering
-    paramText = Enum.map(Map.keys(rgen_params),fn(x) -> ~s/#{x}: #{Map.get(rgen_params,x)}/ end)
-      |> Enum.join(", ")
+    # paramUrl =  Enum.map(Map.keys(rgen_params),fn(x) -> ~s/#{x}=#{Map.get(rgen_params,x)}/ end)
+    #   |> Enum.join("&")
+    redirect(conn, to: util_path(conn, :rgen, distribution: Map.get(rgen_params,"distribution"), size: Map.get(rgen_params,"size")))
+
+  end
+  def rgen(conn, %{"distribution" => distribution, "size" => size}) do
+    paramText = "Distribution: #{distribution}, size: #{size}"
     output = [3,2,1,4,5,6,6,3,3,2]
-    outputText = Enum.join(output, ", ")
+    outputText = "(No, it's not real.) " <> Enum.join(output, ", ")
     render conn, "rgen-output.html", %{"output": outputText , "params": paramText}
   end
-
   def rgen(conn, _params) do
     render conn, "rgen.html"
   end
