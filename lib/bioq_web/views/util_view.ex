59755af63e00d84bcfede27ce654324db893b598
Trang Tran <ttdtrang@gmail.com>
2018-06-17 16:30:04 -0600

    params captured

--- lib/bioq_web/views/util_view.ex
+++ lib/bioq_web/views/util_view.ex
@@ -7,5 +7,22 @@ defmodule BioqWeb.UtilView do
   #   outputText = "(No, it's not real.) " <> Enum.join(output, ", ")
   #   # render "rgen-output.html", %{"output": outputText , "params": paramText}
   # end
+  def rgen_params([:param = %{"distribution" => distribution, "size" => size}]) do
+    paramText = "Distribution: #{distribution}, size: #{size}"
+    paramText
+  end
+  def rgen_params(%{:params => theParams}) do
+    # Map.keys(theParams)
+    # |> Enum.map(fn(x) -> ~s/#{x} = #{Map.get(theParams,x)}/ end)
+    # |> Enum.join(" </br> ")
+    theParams
+  end
 
+  def rgen_params(allAssigns) do
+    "is_list = #{is_list(allAssigns)}" <> "\n" <> "is_map = #{is_map(allAssigns)}"
+  end
+
+  def rgen_output(%{:output => output}) do
+    output
+  end
 end
