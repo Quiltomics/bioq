90e15604fe9c69280aa6a85c6f8ceb33f44920c8
Trang Tran <ttdtrang@gmail.com>
2018-06-18 11:35:45 -0600

    cleaned up

--- lib/bioq_web/views/util_view.ex
+++ lib/bioq_web/views/util_view.ex
@@ -1,16 +1,6 @@
 defmodule BioqWeb.UtilView do
   use BioqWeb, :view
 
-  # def render_rgen_output(%{"distribution" => distribution, "size" => size}) do
-  #   paramText = "Distribution: #{distribution}, size: #{size}"
-  #   output = [3,2,1,4,5,6,6,3,3,2]
-  #   outputText = "(No, it's not real.) " <> Enum.join(output, ", ")
-  #   # render "rgen-output.html", %{"output": outputText , "params": paramText}
-  # end
-  def rgen_params([:param = %{"distribution" => distribution, "size" => size}]) do
-    paramText = "Distribution: #{distribution}, size: #{size}"
-    paramText
-  end
   def rgen_params(%{:params => theParams}) do
     # Map.keys(theParams)
     # |> Enum.map(fn(x) -> ~s/#{x} = #{Map.get(theParams,x)}/ end)
